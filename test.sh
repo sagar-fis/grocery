72cf965f3cd08f67dbdc1b681654e0d5a3d5f6c1
sagar-fis <58681582+sagar-fis@users.noreply.github.com>
2020-09-17 17:25:59 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 echo "This is test filefor checking connectivity between"
 echo "********* GIT and Jenkins *********"
 echo " latest changes in prog to test in jenkins"
-echo "With NGROK"
+echo " With NGROK"
 echo "Time is $(date)"
